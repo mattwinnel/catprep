b30fff44e74e79db021f52ebf1293a0f400bb42f
mattwinnel <96177228+mattwinnel@users.noreply.github.com>
2023-11-18 10:55:12 +0000

    Update run_me.m

--- run_me.m
+++ run_me.m
@@ -3,17 +3,17 @@ dim = 30; % dim is the truncation Fock number, i.e., dim = 1 for qubits
 
 % scheme I
 
-n = 14;
-ROUND_MAX = 6;
-squeezing = 8; % dB.
-eta_total = 0.25;
+n = 14; % input Fock state size
+ROUND_MAX = 6; % number of rounds k
+squeezing = 8; % amount of squeezing in dB
+eta_total = 0.25; % total transmissivity experienced by the input Fock state
 
-
+% compute the output state vector for iterative scheme I
 [ psi_out_scheme_I ] = scheme_I_function(n,dim,eta_total,squeezing,ROUND_MAX);
 
 
-
-dim_trun = 16;
+% plot Wigner function of output state
+dim_trun = 16; % truncate the output state for faster Wigner function calculation
 figure
 wigner_function2D(psi_out_scheme_I(1:dim_trun+1)*psi_out_scheme_I(1:dim_trun+1)',dim_trun)
 drawnow
@@ -21,16 +21,20 @@ disp('displaying output cat for scheme I')
 disp('press any key to continue')
 pause
 
-n = 14;
-ROUND_MAX = 100;
-squeezing_vacuum = 6; % dB.
-squeezing_Fock = 0; % dB.
-eta_total = 0.5;
+n = 14; % input Fock state size
+ROUND_MAX = 100; % number of rounds k
+squeezing_vacuum = 6; % amount of squeezing for squeezed vacuum state in dB
+squeezing_Fock = 0; % amount of initial squeezing on the input state in dB
+eta_total = 0.5; % total transmissivity experienced by the input state
 
 
-
+% compute the output state vector for iterative scheme II
 [ psi_out_scheme_II ] = scheme_II_function(n,dim,eta_total,squeezing_vacuum,squeezing_Fock,ROUND_MAX);
 
+% note that scheme II works for any input state with odd/even Fock-number parity - make changes to "scheme_II_function" to try
+
+% plot Wigner function of output state
+% truncate the output state for faster Wigner function calculation
 figure
 wigner_function2D(psi_out_scheme_II(1:dim_trun+1)*psi_out_scheme_II(1:dim_trun+1)',dim_trun)
 drawnow
